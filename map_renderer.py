e71cf33cb4534aadff19c5c898df2d5374a01e6e
Igor Yeryomin <yeryomin.igor@gmail.com>
2013-12-27 15:56:22 +0200

    Little render changes
    Signed-off-by: Igor Yeryomin <yeryomin.igor@gmail.com>

--- map_renderer.py
+++ map_renderer.py
@@ -4,22 +4,32 @@ from visual import scene, curve, box, color, arrow, label
 class VPythonRenderer():
 
     def draw_axes(self):
-        # scene.forward = (0, 0, -1) # Default direction
-        pointerX = arrow(pos=(0,0,0), axis=(5,0,0), shaftwidth=0.5, color=color.red)
-        label(pos=(5,0,0), text='X')
-        pointerY = arrow(pos=(0,0,0), axis=(0,5,0), shaftwidth=0.5, color=color.blue)
-        label(pos=(0,5,0), text='Y')
-        pointerZ = arrow(pos=(0,0,0), axis=(0,0,5), shaftwidth=0.5, color=color.yellow)
-        label(pos=(0,0,5), text='Z')
+        # scene.forward = (0, 0, -1) # Default view direction
+        zero = (0, 0, 0)
+        x_direction = (5, 0, 0)
+        y_direction = (0, 5, 0)
+        z_direction = (0, 0, 5)
+
+        pointerX = arrow(pos=zero, axis=x_direction,
+                         shaftwidth=0.5, color=color.red)
+        labelX = label(pos=x_direction, text='X')
+        
+        pointerY = arrow(pos=zero, axis=y_direction,
+                         shaftwidth=0.5, color=color.blue)
+        labelY = label(pos=y_direction, text='Y')
+        
+        pointerZ = arrow(pos=zero, axis=z_direction,
+                         shaftwidth=0.5, color=color.yellow)
+        labelZ = label(pos=z_direction, text='Z')
 
     def draw_grid(self, width, length):
         scene.center = (int(width / 2), int(length / 2))
-        scene.forward = (0, 0, -1) # Default direction
-        for i in range(-5, width * 10 + 5, 10):
-            curve(pos=[(i / 10 + 0.5, -0.5), (i / 10 + 0.5, length - 0.5)],
+        scene.forward = (0, 0, -1) # Default view direction
+        for x in range(-5, width * 10 + 5, 10):
+            curve(pos=[(x / 10 + 0.5, -0.5), (x / 10 + 0.5, length - 0.5)],
                   color=color.green)
-        for i in range(-5, length * 10 + 5, 10):
-            curve(pos=[(-0.5, i / 10 + 0.5), (width - 0.5, i / 10 + 0.5)],
+        for y in range(-5, length * 10 + 5, 10):
+            curve(pos=[(-0.5, y / 10 + 0.5), (width - 0.5, y / 10 + 0.5)],
                   color=color.green)
 
     def draw_walls(self, cells):
@@ -30,13 +40,12 @@ class VPythonRenderer():
 
     def draw_path(self, path):
         for cell in path:
-            self.draw_cell(cell)
+            self.draw_cell(cell, color.green)
             
-    def draw_calculated_cells(self, closed, open):
-        for cell in closed:
+    def draw_calculated_cells(self, closed_list, open_list):
+        for cell in closed_list:
             self.draw_cell(cell, color.yellow)
-        
-        for cell in open:
+        for cell in open_list:
             self.draw_cell(cell, color.cyan)
                     
     def draw_start(self, start):
@@ -46,11 +55,5 @@ class VPythonRenderer():
         self.draw_cell(finish, color.red)
         
     def draw_cell(self, cell, clr):
-        self.draw_box(cell, clr)
-        
-    def draw_box(self, point, clr):
-        box(pos=(point.x, point.y, 0.5),
-            width=1,
-            length=1,
-            heigth=1,
-            color=clr)
+        box(pos=(cell.point.x, cell.point.y, 0.5),
+            width=1, length=1, heigth=1, color=clr)
